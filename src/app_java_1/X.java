cb47c01f6280051afb9a26c1f8ca3f53f97a495d
SwetaSharma16591 <sweta.sharma16591@gmail.com>
2021-10-03 21:59:37 +0530

    first commit

---
+++ src/app_java_1/X.java
@@ -0,0 +1,13 @@
+package app_java_1;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.By;
+
+public class X {
+
+	public static void main(String[] args) {
+		
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("https://www.orangehrm.com/hris-hr-software-demo/");
+
+}
